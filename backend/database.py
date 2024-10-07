981a3187781a00b16811e7d61e5f1adf25ff0651
lcw3176 <lcw3176@naver.com>
2024-10-07 16:16:40 +0900

    커밋

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import dotenv_values
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+config = dotenv_values(".env")
+
+SQLALCHEMY_DATABASE_URL = config["DB_URL"]
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
